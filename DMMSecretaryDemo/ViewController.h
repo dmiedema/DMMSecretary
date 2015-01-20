535ff4aa59ce35f9d13eb6106d68adf5ed270b64
dmiedema <daniel@househappy.org>
2015-01-19 17:48:53 -0800

    Initial Commit

---
+++ DMMSecretaryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMMSecretaryDemo
+//
+//  Created by Daniel on 1/19/15.
+//  Copyright (c) 2015 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DMMSecretaryDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DMMSecretaryDemo
+//
+//  Created by Daniel on 1/19/15.
+//  Copyright (c) 2015 Daniel Miedema. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
